ba97fdb4f055cb08c582fa72ef1917ae66a5d622
Chensy <chensy@sharing8.cn>
2018-06-27 19:31:28 +0800

    html依赖

--- app/page/demo/index.js
+++ app/page/demo/index.js
@@ -1,3 +1,9 @@
 /**
  * Created by Chen on 2018-06-27 0027.
  */
+import './style.less'
+import './index.html'
+
+if (module.hot) {
+    module.hot.accept()
+}
--- app/page/discover/index.js
+++ app/page/discover/index.js
@@ -1,5 +1,8 @@
-// import less from './style.less'
+/**
+ * Created by Chen on 2018-06-27 0027.
+ */
 import './style.less'
+import './index.html'
 
 if (module.hot) {
     module.hot.accept()
